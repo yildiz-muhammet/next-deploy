95f888d4b36894e81933dd7605274fcf6e5a1ed4
Muhammet Yildiz <muhammet74ikv@gmail.com>
2024-05-21 15:33:35 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,8 @@ const nextConfig = {
             styledComponents: true
         },
         env: {
-            NEXT_PUBLIC_API_URL: "http://localhost:3000",
+            // NEXT_PUBLIC_API_URL: "http://localhost:3000",
+            NEXT_PUBLIC_API_URL: "https://worderbee.netlify.app",
         }
     }
     // const withNextIntl = require('next-intl/plugin')(
