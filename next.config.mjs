137545b1112ac26ee94f94f8da044cb1ddc0f0ad
Muhammet Yildiz <muhammet74ikv@gmail.com>
2024-05-24 11:21:25 +0300

    light & dark mode and global tint color 🌈

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,13 @@
 /** @type {import('next').NextConfig} */
 import withNextIntl from 'next-intl/plugin';
 const nextConfig = {
-        reactStrictMode: true,
-        compiler: {
-            styledComponents: true
-        },
-        env: {
-            // NEXT_PUBLIC_API_URL: "http://localhost:3000",
-            NEXT_PUBLIC_API_URL: "https://worderbee.netlify.app/",
-        }
+    reactStrictMode: true,
+    compiler: {
+        styledComponents: true
+    },
+    env: {
+        NEXT_PUBLIC_API_URL: "http://localhost:3000/",
     }
-    // const withNextIntl = require('next-intl/plugin')(
-    //     './src/i18n.ts'
-    // );
-    // module.exports = withNextIntl(nextConfig)
-
-
+}
 
 export default withNextIntl('./src/i18n.ts')(nextConfig);
